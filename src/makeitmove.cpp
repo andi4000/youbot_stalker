ac4dd64cc900c279e2038fb2f5d2a18b67a5feb0
ariandyy <ariandyy@gmail.com>
2013-03-27 18:22:17 +0100

    ROS Subscriber finished, ready for control

--- src/makeitmove.cpp
+++ src/makeitmove.cpp
@@ -1,7 +1,32 @@
-#include "ros/ros.h"
+#include "youbotController.h"
+// Ref: http://ibotics.ucsd.edu/trac/stingray/wiki/ROSNodeTutorialC%2B%2B
 
 int main(int argc, char** argv)
 {
-	ROS_INFO("Hello World!");
+	ros::init(argc, argv, "makeitmove");
+	ROS_INFO("hello");
+	ros::NodeHandle n;
+	
+	YoubotController* yb = new YoubotController();
+	
+	ros::Subscriber sub_ObjDetect = n.subscribe("object_tracking/object_detected", 1000, &YoubotController::callbackObjDetected, yb);
+	ros::Subscriber sub_PosX = n.subscribe("object_tracking/x_pos", 1000, &YoubotController::callbackPosX, yb);
+	ros::Subscriber sub_PosY = n.subscribe("object_tracking/y_pos", 1000, &YoubotController::callbackPosY, yb);
+	ros::Subscriber sub_area = n.subscribe("object_tracking/area", 1000, &YoubotController::callbackArea, yb);
+	ros::Rate r(50);
+	
+	
+	while(n.ok()){
+		if (yb->isObjectDetected()){
+			ROS_INFO("(x, y) = (%d, %d)", yb->getObjX(), yb->getObjY());
+			ROS_INFO("area   = %.2f", yb->getObjArea());
+		} else {
+			ROS_INFO("nothing");
+		}		
+		
+		ros::spinOnce();
+		r.sleep();
+	}
+	
 	return 0;
 }
---
+++ src/youbotController.h
@@ -0,0 +1,27 @@
+#include "ros/ros.h"
+
+#include "std_msgs/Bool.h"
+#include "std_msgs/Int32.h"
+#include "std_msgs/Float32.h"
+
+class YoubotController
+{
+	public:
+		YoubotController();
+		~YoubotController();
+		
+		void callbackObjDetected(const std_msgs::Bool::ConstPtr& msg);
+		void callbackPosX(const std_msgs::Int32::ConstPtr& msg);
+		void callbackPosY(const std_msgs::Int32::ConstPtr& msg);
+		void callbackArea(const std_msgs::Float32::ConstPtr& msg);
+		
+		bool isObjectDetected();
+		int getObjX();
+		int getObjY();
+		double getObjArea();
+		
+	private:
+		bool camObjDetected;
+		int camPosX, camPosY;
+		double camArea;
+};
