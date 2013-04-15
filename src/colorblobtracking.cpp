afdae182133380e6d2c06340bda4e346420ab3be
ariandyy <ariandyy@gmail.com>
2013-04-15 13:17:51 +0200

    meh

--- src/colorblobtracking.cpp
+++ src/colorblobtracking.cpp
@@ -10,7 +10,6 @@
 
 /**
  * //TODO
- * - put everything into modular functions!
  * - develop better image recognition!!
  * 
  */
@@ -29,6 +28,67 @@ Mat getHSVThresholdedImg(const Mat& src, int lo_h, int lo_s, int lo_v, int hi_h,
 	erode(matThresh, matThresh, Mat());
 	
 	return matThresh;
+}
+
+void doContourProcessing(const Mat& matSrc, const Mat& matThresh, Point& out_centroid, bool& out_gotIt, double& out_maxArea, Mat& out_mat)
+{
+	double minimalArea = 2000;
+	Mat matOutput(matSrc);
+	
+	vector< vector<Point> > contours;
+	findContours(matThresh, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
+	
+	vector<double> areas(contours.size());
+	for (unsigned int i = 0; i < contours.size(); i++)
+		areas[i] = contourArea(Mat(contours[i]));
+	
+	double max_area;
+	Point maxPosition;
+	minMaxLoc(Mat(areas), 0, &max_area, 0, &maxPosition);
+	//ROS_INFO("biggest area = %f", max_area);
+	
+	bool got_it = false;
+	if (max_area > minimalArea) got_it = true;
+	
+	if (got_it)
+		ROS_INFO("got it");
+	else
+		ROS_INFO("nothing");
+		
+	// draw only the biggest contour
+	drawContours(matOutput, contours, maxPosition.y, Scalar(0, 0, 255), CV_FILLED);
+	
+	// GETTING THE CENTROID BEGIN
+	Point image_centroid(0, 0);
+	Point relative_centroid(0, 0);
+	if (got_it){
+		vector<Moments> mu(contours.size());
+		for ( unsigned int i = 0; i < contours.size(); i++ ) {
+			mu[i] = moments(contours[i], false);
+		}
+		
+		vector<Point> mc(contours.size());
+		for ( unsigned int i = 0; i < contours.size(); i++ ) {
+			mc[i] = Point( mu[i].m10/mu[i].m00, mu[i].m01/mu[i].m00 );
+		}
+		
+		// draw the centroid of the biggest contour
+		image_centroid = mc[maxPosition.y];
+		circle(matOutput, image_centroid, 4, Scalar(255, 0, 0), -1, 8, 0);
+		//ROS_INFO("image centroid (x, y) = (%d, %d)", image_centroid.x, image_centroid.y);
+		
+		// getting the relative centroid (middle point as 0,0)
+		relative_centroid.x = image_centroid.x - (matSrc.cols / 2);
+		relative_centroid.y = -image_centroid.y + (matSrc.rows / 2);
+		ROS_INFO("relative centroid (x, y) = (%d, %d) area %.2f", relative_centroid.x, relative_centroid.y, max_area);
+	}
+	// GETTING THE CENTROID END
+	
+	//TODO: this is ugly
+	out_centroid = relative_centroid;
+	out_maxArea = max_area;
+	out_gotIt = got_it;
+	out_mat = matOutput;
 }
 
 int main(int argc, char** argv)
@@ -54,10 +114,11 @@ int main(int argc, char** argv)
 		ROS_INFO("Opening /dev/video%d", camIndex);
 	}
 	
-	ROS_INFO("To display image, use option \"-d\"");
 	bool displayWindows = false;
 	if (argv[1] && strcmp(argv[1], "-d") == 0)
 		displayWindows = true;
+	else
+		ROS_INFO("To display image, use option \"-d\"");
 	
 	// ROS MESSAGE BEGIN
 	// Ref: http://www.ros.org/wiki/ROS/Tutorials/WritingPublisherSubscriber%28c%2B%2B%29
@@ -74,22 +135,22 @@ int main(int argc, char** argv)
 	int capSizeY = 480;
 	
 	if (displayWindows){
-		namedWindow("Webcam", CV_WINDOW_AUTOSIZE);
 		namedWindow("Output", CV_WINDOW_AUTOSIZE);
-		moveWindow("Webcam", 0*capSizeX, 0);
-		moveWindow("Output", 1*capSizeX, 0);
+		namedWindow("Threshold", CV_WINDOW_AUTOSIZE);
+		moveWindow("Output", 0*capSizeX, 0);
+		moveWindow("Threshold", 1*capSizeX, 0);
 	}
 
 	// HSV max and min values in opencv
 	//int lo_h = 0, lo_s = 0, lo_v = 0, hi_h = 180, hi_s = 255, hi_v = 255;
 	// this is for green
 	int lo_h = 23, lo_s = 68, lo_v = 81, hi_h = 60, hi_s = 174, hi_v = 228;
-	createTrackbar("Lo H", "Output", &lo_h, 180);
-	createTrackbar("Lo S", "Output", &lo_s, 255);
-	createTrackbar("Lo V", "Output", &lo_v, 255);
-	createTrackbar("Hi H", "Output", &hi_h, 180);
-	createTrackbar("Hi S", "Output", &hi_s, 255);
-	createTrackbar("Hi V", "Output", &hi_v, 255);
+	createTrackbar("Lo H", "Threshold", &lo_h, 180);
+	createTrackbar("Lo S", "Threshold", &lo_s, 255);
+	createTrackbar("Lo V", "Threshold", &lo_v, 255);
+	createTrackbar("Hi H", "Threshold", &hi_h, 180);
+	createTrackbar("Hi S", "Threshold", &hi_s, 255);
+	createTrackbar("Hi V", "Threshold", &hi_v, 255);
 	
 	
 	// ROS SIGINT handler
@@ -100,64 +161,18 @@ int main(int argc, char** argv)
 		
 		Mat matThresh;
 		matThresh = getHSVThresholdedImg(srcFrame, lo_h, lo_s, lo_v, hi_h, hi_s, hi_v);
-		// CONTOUR TEST PART2 BEGIN
-		// source: http://www.bytefish.de/blog/extracting_contours_with_opencv
-		//Mat threshMat(threshImg);
-		//Mat srcMat(srcImg);
 		
-		vector< vector<Point> > contours;
-		findContours(matThresh, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
-		
-		vector<double> areas(contours.size());
-		for (unsigned int i = 0; i < contours.size(); i++)
-			areas[i] = contourArea(Mat(contours[i]));
-		
-		double max_area;
-		Point maxPosition;
-		minMaxLoc(Mat(areas), 0, &max_area, 0, &maxPosition);
-		//ROS_INFO("biggest area = %f", max_area);
-		
-		bool got_it = false;
-		if (max_area > 2000) got_it = true;
-		
-		if (got_it)
-			ROS_INFO("got it");
-		else
-			ROS_INFO("nothing");
-		// draw only the biggest contour
-		drawContours(srcFrame, contours, maxPosition.y, Scalar(0, 0, 255), CV_FILLED);
-		
-		// GETTING THE CENTROID BEGIN
-		Point image_centroid(0, 0);
+		Mat matOutput;
 		Point relative_centroid(0, 0);
-		if (got_it){
-			vector<Moments> mu(contours.size());
-			for ( unsigned int i = 0; i < contours.size(); i++ ) {
-				mu[i] = moments(contours[i], false);
-			}
-			
-			vector<Point> mc(contours.size());
-			for ( unsigned int i = 0; i < contours.size(); i++ ) {
-				mc[i] = Point( mu[i].m10/mu[i].m00, mu[i].m01/mu[i].m00 );
-			}
-			
-			// draw the centroid of the biggest contour
-			image_centroid = mc[maxPosition.y];
-			circle(srcFrame, image_centroid, 4, Scalar(255, 0, 0), -1, 8, 0);
-			//ROS_INFO("image centroid (x, y) = (%d, %d)", image_centroid.x, image_centroid.y);
-			
-			// getting the relative centroid (middle point as 0,0)
-			relative_centroid.x = image_centroid.x - (capSizeX / 2);
-			relative_centroid.y = -image_centroid.y + (capSizeY / 2);
-			ROS_INFO("relative centroid (x, y) = (%d, %d) area %.2f", relative_centroid.x, relative_centroid.y, max_area);
-		}
-		// GETTING THE CENTROID END
+		double max_area;
+		bool got_it;
 		
+		doContourProcessing(srcFrame, matThresh, relative_centroid, got_it, max_area, matOutput); 
+	
 		if (displayWindows){
-			imshow("Output", matThresh);
-			imshow("Webcam", srcFrame);
+			imshow("Output", matOutput);
+			imshow("Threshold", matThresh);
 		}
-		// CONTOUR TEST PART2 END
 				
 		// ROS MESSAGE BEGIN
 		std_msgs::Bool msg_obj_det;
