71a5d11d63aeceed709e661f6207526d1eb87da6
ariandyy <ariandyy@gmail.com>
2013-04-30 17:13:15 +0200

    added some reading sources

---
+++ src/humantracking.cpp
@@ -0,0 +1,16 @@
+#include "ros/ros.h"
+
+/**
+ * //TODO:
+ * - read /tf topic values for human skeleton
+ * - Ref: http://answers.ros.org/question/10515/openni_trackertransform-depth/
+ * - Ref: http://answers.ros.org/question/35327/accurate-distance-to-kinect-with-openni/
+ * - calculate distance
+ * - recognise gestures
+ * 
+ */
+ 
+int main(int argc, char** argv){
+	
+	return 0;
+}
