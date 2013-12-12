dd431b7cfdc1cf8b6eb6616d8b5d60a6d2ee1af0
ariandyy <ariandyy@gmail.com>
2013-12-12 21:11:31 +0100

    increased sensitivity for x gesture

--- src/makeitmove.cpp
+++ src/makeitmove.cpp
@@ -206,7 +206,7 @@ int main(int argc, char** argv)
 			float gestureOffsetY = 0;
 			
 			if (yb->getGestureState()){
-				gestureOffsetX = 1200*yb->getRobotOffsetX();
+				gestureOffsetX = 1300*yb->getRobotOffsetX();
 				gestureOffsetY = 0.6*yb->getRobotOffsetY();
 			}
 			
